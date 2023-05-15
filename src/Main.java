cddd2a3d5318d76ff73a4806f1de8e5d5f6a67b7
Krestego <mazaeva.daria.v@gmail.com>
2023-05-15 12:30:37 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 7489;
+        int a = 20;
+        int bonus = (price/a);
+        System.out.println("Стоимость покупки = " + price + " руб. ");
+        System.out.println("Мили за покупку = " + bonus + " шт. ");
+                }
+    }
